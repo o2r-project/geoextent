017f46c480bda1a4718ab264c3a8f46a7008971e
YouQam <qamazyousef@gmail.com>
2019-09-09 00:28:05 +0200

    update requested changes

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,119 +1,62 @@
 import os           # used to get the location of the testdata
-import pygeoj       # used to parse the geojson file
-import shapefile 
-import xarray as xr
-import sys
+import geoextent
 
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
-###############################
-# Check file existence
-###############################
-def test_geojson_file_exist():
-    filepath=__location__+"/Testdata/muenster_ring_zeit.geojson"
-    assert os.path.isfile(filepath) == True
 
-def test_nc_file_exist():
-    filepath=__location__+"/Testdata/ECMWF_ERA-40_subset1.nc"
-    assert os.path.isfile(filepath) == True
-
-def test_kml_file_exist():
-    filepath=__location__+"/Testdata/aasee.kml"
-    assert os.path.isfile(filepath) == True
-
-def test_tif_file_exist():
-    filepath=__location__+"/Testdata/wf_100m_klas.tif"
-    assert os.path.isfile(filepath) == True
-
-def test_gpkg_file_exist():
-    filepath=__location__+"/Testdata/nc.gpkg"
-    assert os.path.isfile(filepath) == True
-
-def test_csv_file_exist():
-    filepath=__location__+"/Testdata/cities_NL.csv"
-    assert os.path.isfile(filepath) == True
-
-def test_gml_file_exist():
-    filepath=__location__+"/Testdata/clc_1000_PT.gml"
-    assert os.path.isfile(filepath) == True
-
-def test_shp_file_exist():
-    filepath=__location__+"/Testdata/Abgrabungen_Kreis_Kleve_Shape.shp"
-    assert os.path.isfile(filepath) == True
-
-def test_dbf_file_exist():
-    filepath=__location__+"/Testdata/Abgrabungen_Kreis_Kleve_Shape.dbf"
-    assert os.path.isfile(filepath) == True
-
-###############################
-# Check bbox extraction
-###############################
 def test_geojson_extract_bbox():
-    filepath=__location__+"/Testdata/muenster_ring_zeit.geojson"
-    geojson = pygeoj.load(filepath)
-    bbox = (geojson).bbox
-    assert bbox == [7.6016807556152335, 51.94881477206191, 7.647256851196289, 51.974624029877454]
+    filepath=__location__+"/Testdata/folder/muenster_ring_zeit.geojson"
+    assert geoextent.getMetadata(filepath, 'bbox' , True) == [[7.6016807556152335, 51.94881477206191, 7.647256851196289, 51.974624029877454],['2018-11-14']]
 
 def test_invalid_coordinate_geojson_extract_bbox(capsys):
     filepath=__location__+"/Testdata/invalid_coordinate.geojson"
-    sys.stderr.write("error message")
-    print("Invalid geojson file")
-    out, error = capsys.readouterr()
-    assert out == "Invalid geojson file\n"
-    assert error == "error message"
+    assert geoextent.getMetadata(filepath, 'bbox' , True) == [[None],[None]]
+
 
 def test_one_point_geojson_extract_bbox():
     filepath=__location__+"/Testdata/onePoint.geojson"
-    geojson = pygeoj.load(filepath)
-    bbox = (geojson).bbox
-    assert bbox == [6.220493316650391, 50.52150360276628, 6.220493316650391, 50.52150360276628]
+    assert geoextent.getMetadata(filepath, 'bbox' , True) == [[6.220493316650391, 50.52150360276628, 6.220493316650391, 50.52150360276628],[None]]
 
 def test_empty_file_geojson_extract_bbox(capsys):
     filepath=__location__+"/Testdata/empty.geojson"
-    captured = capsys.readouterr()
-    assert captured.out == "Empty file"
+    assert geoextent.getMetadata(filepath, 'bbox' , True) == [[None],[None]]
 
-def test_nc_extract_bbox():
+
+def test_netcdf_extract_bbox():
     filepath=__location__+"/Testdata/ECMWF_ERA-40_subset1.nc"
-    assert netcdf_bbox(filepath) == [0.0, -90.0, 357.5, 90.0]
+    assert geoextent.getMetadata(filepath, 'bbox' , True) == [[-90.0, 0.0, 90.0, 357.5],['2002-07-31']]
+
+
+def test_kml_extract_bbox():
+    filepath=__location__+"/Testdata/aasee.kml"
+    assert geoextent.getMetadata(filepath, 'bbox' , True) == [[7.594213, 51.942466, 7.618246, 51.957278],[None]]
+
+def test_geotiff_extract_bbox():
+    filepath=__location__+"/Testdata/wf_100m_klas.tif"
+    assert geoextent.getMetadata(filepath, 'bbox' , True) == [[5.9153007564753155, 50.31025197410836, 9.468398712484145, 52.5307755328733],[None]]
+
+def test_gpkg_extract_bbox():
+    filepath=__location__+"/Testdata/census2016_cca_qld_short.gpkg"
+    assert geoextent.getMetadata(filepath, 'bbox' , True) == [[-43.7405, 96.8169, -9.14218, 167.998],[None]]
+
+def test_csv_extract_bbox():
+    filepath=__location__+"/Testdata/folder/cities_NL.csv"
+    assert geoextent.getMetadata(filepath, 'bbox' , True) == [[6.574722, 51.434444, 4.3175, 53.217222],['2018-09-30']]]
+
+def test_gml_extract_bbox():
+    filepath=__location__+"/Testdata/clc_1000_PT.gml"
+    assert geoextent.getMetadata(filepath, 'bbox' , True) == [[-17.5420724159224, 32.3966928193202, -6.95938792923511, 39.3011352746141],['2013-11-30T23:00:00Z']]
 
 def test_shapefile_extract_bbox():
     filepath=__location__+"/Testdata/Abgrabungen_Kreis_Kleve_Shape.shp"
-    sf = shapefile.Reader(filepath)
-    output = sf.bbox
-    print(output)
-    assert output == [295896.274870878, 5694747.64703736, 325999.79578122497, 5747140.98659967]
+    assert geoextent.getMetadata(filepath, 'bbox' , True) == [[295896.274870878, 5694747.64703736, 325999.79578122497, 5747140.98659967],[None]]
 
-###############################
-# Functions
-###############################
-"""
-Function for extracting the bbox of netcdf file
+def test_json_extract_bbox():
+    filepath=__location__+"/Testdata/folder/schutzhuetten_aachen.json"
+    assert geoextent.getMetadata(filepath, 'bbox' , True) == [[7.6016807556152335, 51.94881477206191, 7.647256851196289, 51.974624029877454],[None]]
 
-:param filepath: path to the file
-:returns: bounding box of the netCDF in the format [minlon, minlat, maxlon, maxlat]
-"""
-def netcdf_bbox(filepath):
-    ds = xr.open_dataset(filepath)
-    try:
-        lats = ds.coords["lat"]
-        lons = ds.coords["lon"]
-    except Exception as e:
-        try:
-            lats = ds.coords["latitude"]
-            lons = ds.coords["longitude"]
-        except Exception as e:
-            click.echo(e)
-
-    min_lat=min(lats).values
-    min_lat_float=float(min_lat)
-    min_lon=min(lons).values
-    min_lon_float=float(min_lon)
-    max_lat=max(lats).values
-    max_lat_float=float(max_lat)
-    max_lon=max(lons).values
-    max_lon_float=float(max_lon)
-
-    bbox = [min_lon_float,min_lat_float,max_lon_float,max_lat_float]
-    ds.close()
-    return bbox
+def test_folder_multiple_files():
+    filepath=__location__+"/Testdata/folder"
+    assert [7.6016807556152335, 51.94881477206191, 7.647256851196289, 51.974624029877454] in geoextent.getMetadata(filepath, 'bbox' , True)
+    assert [6.574722, 51.434444, 4.3175, 53.217222] in geoextent.getMetadata(filepath, 'bbox' , True)
+    assert [292063.81225905, 5618144.09259115, 302531.3161606, 5631223.82854667] in geoextent.getMetadata(filepath, 'bbox' , True)
