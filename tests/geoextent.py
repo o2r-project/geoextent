8c866a370ed9380c5f8070f3376aa9f3670069bf
YouQam <qamazyousef@gmail.com>
2019-09-23 11:54:48 +0200

    improve getmetadata fun.

--- tests/geoextent.py
+++ tests/geoextent.py
@@ -1,8 +1,8 @@
 def getMetadata(path, bbox=False, time = False):
-    if bbox == 'bbox' and time == True :
+    if bbox == 'bbox' and time == 'Time' :
         return "dummy bbox and time"
     elif bbox == 'bbox' and time == False :
         return "dummy bbox"
-    elif bbox == False and time == True :
+    elif bbox == False and time == 'Time' :
         return "dummy time"
     
