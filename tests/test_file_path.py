2bf7c52d647b04d1d249a9a38c91a4a831bcd446
YouQam <qamazyousef@gmail.com>
2019-11-28 23:43:39 +0100

    #31: Adding test file to test relative file path.

--- tests/test_file_path.py
+++ tests/test_file_path.py
@@ -2,7 +2,7 @@ import pytest
 import geoextent.lib.helpfunctions as hf
 
 def test_check_relative_file_path():
-    assert hf.checkPath("muenster_ring_zeit.geojson")
+    assert hf.checkPath("relative.geojson")
 
 def test_check_abs_file_path():
     assert hf.checkPath("/testdata/muenster_ring_zeit.geojson")
