cf7d929d36c88a14cd2f6432678b3a2db863228d
YouQam <qamazyousef@gmail.com>
2019-12-17 07:59:34 +0100

    #34: adding tests  for different delimiters.

--- tests/test_api_csv.py
+++ tests/test_api_csv.py
@@ -25,3 +25,17 @@ def test_empty_csv_file():
     assert "bbox" not in result
     assert "tbox" not in result
     assert "crs" not in result
+
+def test_csv_extract_bbox_and_tbox_semicolon_delimiter():
+    result = geoextent.fromFile('testdata/csv/csv_semicolon_delimiter.csv', bbox=True, tbox=True)
+    assert "bbox" in result
+    assert "tbox" in result
+    assert result["bbox"] == [4.3175, 51.434444, 6.574722, 53.217222]
+    assert result["tbox"] == ['01.08.2017', '30.09.2019']
+
+def test_csv_extract_bbox_and_tbox_comma_delimiter():
+    result = geoextent.fromFile('testdata/csv/csv_comma_delimiter.csv', bbox=True, tbox=True)
+    assert "bbox" in result
+    assert "tbox" in result
+    assert result["bbox"] == [4.3175, 51.434444, 6.574722, 53.217222]
+    assert result["tbox"] == ['01.08.2017', '30.09.2019']
