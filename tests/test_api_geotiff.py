69ada40aef07077eda410f318c3a9a7823e990be
YouQam <qamazyousef@gmail.com>
2020-02-25 10:48:04 +0100

    update geotiff test

--- tests/test_api_geotiff.py
+++ tests/test_api_geotiff.py
@@ -2,7 +2,6 @@ import geoextent.lib.extent as geoextent
 
 def test_geotiff_extract_bbox():
     result = geoextent.fromFile('testdata/tif/wf_100m_klas.tif', bbox=True)
-    assert "bbox" in result
     assert result["bbox"] == [5.91530075647532, 50.3102519741084, 9.46839871248415, 52.5307755328733]
 
 def test_geotiff_extract_time():
@@ -11,5 +10,4 @@ def test_geotiff_extract_time():
 
 def test_geotiff_crs_used():
     result = geoextent.fromFile('testdata/tif/wf_100m_klas.tif', bbox=True)
-    assert "crs" in result
     assert result["crs"] == '4326'
