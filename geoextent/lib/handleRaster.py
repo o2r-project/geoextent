43d68adee4a66b5743a359b18a5cde720ce10721
sbastiangarzon <js.garzon398@uniandes.edu.co>
2020-12-17 16:03:05 +0100

    Simplify transformation

--- geoextent/lib/handleRaster.py
+++ geoextent/lib/handleRaster.py
@@ -38,52 +38,42 @@ def getBoundingBox(filePath):
     input "filepath": type string, file path to geotiff file \n
     returns bounding box of the file: type list, length = 4 , type = float, schema = [min(longs), min(lats), max(longs), max(lats)] 
     '''
-
     # Enable exceptions
     gdal.UseExceptions()
 
     geotiffContent = gdal.Open(filePath)
 
-    #get the existing coordinate system
-    old_cs= osr.SpatialReference()
+    # get the existing coordinate system
+    old_cs = osr.SpatialReference()
     old_cs.ImportFromWkt(geotiffContent.GetProjectionRef())
-    
+
     # create the new coordinate system
-    wgs84_wkt = """
-    GEOGCS["WGS 84",
-        DATUM["WGS_1984",
-            SPHEROID["WGS 84",6378137,298.257223563,
-                AUTHORITY["EPSG","7030"]],
-            AUTHORITY["EPSG","6326"]],
-        PRIMEM["Greenwich",0,
-            AUTHORITY["EPSG","8901"]],
-        UNIT["degree",0.01745329251994328,
-            AUTHORITY["EPSG","9122"]],
-        AUTHORITY["EPSG","4326"]]"""
+
     new_cs = osr.SpatialReference()
-    new_cs.ImportFromWkt(wgs84_wkt)
+    new_cs.ImportFromEPSG(hf.WGS84_EPSG_ID)
 
     # create a transform object to convert between coordinate systems
-    transform = osr.CoordinateTransformation(old_cs,new_cs)
-    
-    #get the point to transform, pixel (0,0) in this case
+    transform = osr.CoordinateTransformation(old_cs, new_cs)
+
+    # get the point to transform, pixel (0,0) in this case
     width = geotiffContent.RasterXSize
     height = geotiffContent.RasterYSize
     gt = geotiffContent.GetGeoTransform()
 
     minx = gt[0]
-    miny = gt[3] + width*gt[4] + height*gt[5]
-    maxx = gt[0] + width*gt[1] + height*gt[2]
+    miny = gt[3] + width * gt[4] + height * gt[5]
+    maxx = gt[0] + width * gt[1] + height * gt[2]
     maxy = gt[3]
 
-    #get the coordinates in lat long
-    latlongmin = transform.TransformPoint(minx,miny)
-    latlongmax = transform.TransformPoint(maxx,maxy)
+    # get the coordinates in lat long
+    latlongmin = transform.TransformPoint(minx, miny)
+    latlongmax = transform.TransformPoint(maxx, maxy)
 
-    #get the bBox
+    # get the bBox
     bbox = [latlongmin[0], latlongmin[1], latlongmax[0], latlongmax[1]]
 
     return bbox
+
 
 def getCRS(filePath):
     ''' gets the coordinate reference systems from the geotiff file \n
@@ -91,7 +81,7 @@ def getCRS(filePath):
     return epsg code of the used coordiante reference system: type int
     '''
 
-    return '4326'
+    return "4326"
 
 def getTemporalExtent(filePath):
     ''' extracts temporal extent of the geotiff \n
